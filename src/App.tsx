96f440108946165bf70ec7310445987c4188a1b7
shengwendi <shengwendi@gmail.com>
2024-03-01 13:27:29 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,14 +23,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-        测试后
 
         <div className="card">
             <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
